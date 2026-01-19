a630c6efaa5d966edb4a5124ca3bf9aae81c7a3d
zhouhui0802 <1741484908@qq.com>
2026-01-19 13:55:33 +0800

    knife4j的使用 采用的是分组

---
+++ knife4j_demo/src/main/java/com/zh/SwaggerDemoApplication.java
@@ -0,0 +1,18 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SwaggerDemoApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SwaggerDemoApplication.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
