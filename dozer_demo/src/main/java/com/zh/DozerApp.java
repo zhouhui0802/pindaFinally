b4c182d5ee957ff628e11a2f70073621f652c273
zhouhui0802 <1741484908@qq.com>
2026-01-19 14:50:06 +0800

    dozer类属性的赋值使用

---
+++ dozer_demo/src/main/java/com/zh/DozerApp.java
@@ -0,0 +1,18 @@
+package com.zh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DozerApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(DozerApp.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dozer_demo/src/main/java/com/zh/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.zh.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String userId;
+
+    private String userName;
+
+    private int userAge;
+
+    private String address;
+
+    private String birthday;
+}
